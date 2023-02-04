a0ed01f1d494b648d11cb091c0752c27af7226bd
lizhi <lizhi@aijiaoyi.xyz>
2023-02-04 23:27:05 +0800

    feat: home 页

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,54 @@
-import { FunctionComponent } from "react";
-
+import { FunctionComponent, useState } from "react";
+import avatar from '@/assets/imgs/avatar.jpeg';
+import expand from '@/assets/imgs/expand.png';
+import styles from './index.module.scss';
 interface IHomeProps {
 
 }
 
 const Home: FunctionComponent<IHomeProps> = () => {
+
+  const [isShowNav, setIsShowNav] = useState<boolean>(false);
+
+  const handleExpandIconClick = () => {
+    setIsShowNav((bool) => !bool)
+  }
+
   return (
-    <div>
-      home
+    <div className={styles.wrap}>
+      <nav className={`${styles.nav} ${isShowNav ? styles['show-nav'] : ''}`}>
+        <div className={styles['nav-main']}>
+          <div className={styles['avator']}>
+            <img src={avatar} alt="" />
+          </div>
+          <ul>
+            <li>主页</li>
+            <li>相册</li>
+            <li>博客</li>
+          </ul>
+        </div>
+        <div
+          className={styles['show-nav-btn']}
+          onClick={handleExpandIconClick}
+        >
+          <img src={expand} alt="" />
+        </div>
+      </nav>
+      <header>
+        <h1>我的主页</h1>
+        <p>如你, 才华横溢</p>
+        <button id="loginBtn">登录</button>
+      </header>
+      <main>
+        <div className={styles['container']}>
+          <h2>关于我的主页</h2>
+          <p>闲着没事乱写的</p>
+          <h2>啦啦啦</h2>
+          <p>让我们唱一首歌吧</p>
+          <h2>唱什么歌</h2>
+          <p>就唱我们八路军的军歌</p>
+        </div>
+      </main>
     </div>
   );
 }
