892d90042098eda2d41b651729d9ea2123643d1f
lizhi <lizhi@aijiaoyi.xyz>
2023-02-04 22:32:02 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react";
+
+interface IHomeProps {
+
+}
+
+const Home: FunctionComponent<IHomeProps> = () => {
+  return (
+    <div>
+      home
+    </div>
+  );
+}
+
+export default Home;
